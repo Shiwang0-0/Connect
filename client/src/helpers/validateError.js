df1ba3fd912fa19ce20d91945cc346f5b52e3537
Shiwang0-0 <bisht0shiwang@gmail.com>
2024-07-03 08:14:54 +0530

    Validate input error function

---
+++ client/src/helpers/validateError.js
@@ -0,0 +1,11 @@
+export const validateError = (values) => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = "Username is required!";
+    } 
+    if (!values.password) {
+      errors.password = "Password is required";
+    } 
+    return errors;
+  };
+  
